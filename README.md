011ca41312a51272feefb680b1455a20ae863f30
Adam Dubey <adubey0311@gmail.com>
2022-02-15 17:57:50 -0500

    [AD][02M1522] Port rad-poc and initialize project

---
+++ README.md
@@ -0,0 +1,39 @@
+# MERN Ecommerce Bookstore Webapp
+
+A MERN Ecommerce retail application.
+
+---
+
+## Quick Start
+
+_**Back-End (Server):**_
+
+```sh
+# install dependences
+$ npm install
+
+$ npm start
+# in browser, visit localhost:8000/api
+```
+
+---
+
+## Technologies & Frameworks
+
+_**Back-End (Server):**_
+
+- [uuid]()
+- [dotenv]()
+- [Express]()
+- [Node.js]()
+- [Nodemon]()
+- [MongoDB]()
+- [Mongoose]()
+- [body-parser]()
+- [morgan]()
+- [cookie-parser]()
+- [express-validator]()
+- [JWT]()
+- [formidable]()
+- [lodash]()
+- [cors]()
---
+++ server/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const expressValidator = require('express-validator');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Import Routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const productRoutes = require('./routes/product');
+
+
+
+// Database
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+app.use(cors());
+
+// Routes Middleware
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+
+
+
+const port = process.env.PORT || 8000
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}!`)
+})
+
+
