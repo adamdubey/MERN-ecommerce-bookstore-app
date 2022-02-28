923d1256ef0e6c8156953e80bcf2186af6667011
Adam Dubey <adubey0311@gmail.com>
2022-02-28 08:11:08 -0500

    [AD][02M2822] Adding Client Auth functions

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -2,8 +2,10 @@ import React from 'react';
 import Layout from './Layout';
 
 const Home = () => (
-  <Layout title="Home Page" description="MERN E-Commerce Bookstore Home Page">
-  </Layout>
+  <Layout
+    title="Home Page"
+    description="MERN E-Commerce Bookstore Home Page"
+  ></Layout>
 );
 
 export default Home;
--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../core/Layout';
-import { API } from '../config'
+import { signup } from '../auth';
 
 const Signup = () => {
   const [values, setValues] = useState({
@@ -9,84 +9,100 @@ const Signup = () => {
     email: '',
     password: '',
     error: '',
-    success: false
+    success: false,
   });
 
   const { name, email, password, success, error } = values;
 
-  const handleChange = name => event => {
+  const handleChange = (name) => (event) => {
     setValues({ ...values, error: false, [name]: event.target.value });
   };
 
-  const signup = (user) => {
-    return fetch(`${API}/signup`, {
-      method: "POST",
-      headers: {
-        Accept: 'application/json',
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(user)
-    })
-    .then(response => {
-      return response.json()
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  };
-
-  const clickSubmit = event => {
+  const clickSubmit = (event) => {
     event.preventDefault();
-    setValues({ ...values, error: false })
-    signup({ name, email, password })
-      .then(data => {
-        if(data.error) {
-          setValues({ ...values, error: data.error, success: false })
-        } else {
-          setValues({ ...values, name: '', email: '', password: '', error: '', success: true })
-        }
-      })
+    setValues({ ...values, error: false });
+    signup({ name, email, password }).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, success: false });
+      } else {
+        setValues({
+          ...values,
+          name: '',
+          email: '',
+          password: '',
+          error: '',
+          success: true,
+        });
+      }
+    });
   };
 
   const signUpForm = () => (
     <form>
-      <div className='form-group'>
-        <label className='text-muted'>Name</label>
-        <input onChange={handleChange('name')} type="text" className='form-control' value={name} />
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange('name')}
+          type="text"
+          className="form-control"
+          value={name}
+        />
       </div>
 
-      <div className='form-group'>
-        <label className='text-muted'>Email</label>
-        <input onChange={handleChange('email')} type="email" className='form-control' value={email} />
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange('email')}
+          type="email"
+          className="form-control"
+          value={email}
+        />
       </div>
 
-      <div className='form-group'>
-        <label className='text-muted'>Password</label>
-        <input onChange={handleChange('password')} type="password" className='form-control' value={password} />
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange('password')}
+          type="password"
+          className="form-control"
+          value={password}
+        />
       </div>
-      <button onClick={clickSubmit} className='btn btn-primary'>Submit</button>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
     </form>
-  )
+  );
 
   const showError = () => (
-    <div className='alert alert-danger' style={{ display: error ? '' : 'none' }}>{error}</div>
-  )
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? '' : 'none' }}
+    >
+      {error}
+    </div>
+  );
 
   const showSuccess = () => (
-    <div className='alert alert-info' style={{ display: success ? '' : 'none' }}>New account has been created. Please <Link to="/signin">Signin</Link></div>
-  )
+    <div
+      className="alert alert-info"
+      style={{ display: success ? '' : 'none' }}
+    >
+      New account has been created. Please <Link to="/signin">Signin</Link>
+    </div>
+  );
 
   return (
     <Layout
-    title="Signup"
-    description="Signup to MERN E-Commerce Bookstore App"
-    className="container col-md-8 offset-md-2"
-  >
-    {showSuccess()}
-    {showError()}
-    {signUpForm()}
-  </Layout>
-  )
+      title="Signup"
+      description="Signup to MERN E-Commerce Bookstore App"
+      className="container col-md-8 offset-md-2"
+    >
+      {showSuccess()}
+      {showError()}
+      {signUpForm()}
+    </Layout>
+  );
 };
 
 export default Signup;
