1398d32a964063beeea6ae821efc036a3fd7af9d
Adam Dubey <adubey0311@gmail.com>
2022-03-03 08:02:12 -0500

    [AD][03M0322] Creating add new category functionality for admin user

--- client/src/admin/AddCategory.js
+++ client/src/admin/AddCategory.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
+import { createCategory } from './apiAdmin';
 
 const AddCategory = () => {
   // state
@@ -21,7 +22,14 @@ const AddCategory = () => {
     e.preventDefault();
     setError('');
     setSuccess(false);
-    // make req to api to create category
+    createCategory(user._id, token, { name }).then((data) => {
+      if (data.error) {
+        setError(true);
+      } else {
+        setError('');
+        setSuccess(true);
+      }
+    });
   };
 
   const newCategoryForm = () => (
@@ -34,19 +42,51 @@ const AddCategory = () => {
           onChange={handleChange}
           value={name}
           autoFocus
+          required
         />
       </div>
       <button className="btn btn-outline-primary">Create Category</button>
     </form>
   );
 
+  const showSuccess = () => {
+    if (success) {
+      return (
+        <h3 className="text-success">Yay! {name} category has been created!</h3>
+      );
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return (
+        <h3 className="text-danger">
+          Error: {name} category should be unique.
+        </h3>
+      );
+    }
+  };
+
+  const goBack = () => (
+    <div className="mt-5">
+      <Link to="/admin/dashboard" className="text-warning">
+        Back to Dashboard
+      </Link>
+    </div>
+  );
+
   return (
     <Layout
       title="Add a New Category"
-      description={`G'day ${name}, ready to add a new category?`}
+      description={`G'day ${user.name}, ready to add a new category?`}
     >
       <div className="row">
-        <div className="col-md-8 offset-md-2">{newCategoryForm()}</div>
+        <div className="col-md-8 offset-md-2">
+          {showSuccess()}
+          {showError()}
+          {newCategoryForm()}
+          {goBack()}
+        </div>
       </div>
     </Layout>
   );
