cf6c278286c44e8a839c2b5228fb19f0b9ed4f35
Adam Dubey <adubey0311@gmail.com>
2022-03-09 08:35:16 -0500

    [AD][03M0922] Adding client function for search filters

--- client/src/core/Radiobox.js
+++ client/src/core/Radiobox.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, Fragment } from 'react';
 
-const Radiobox = ({ prices }) => {
+const Radiobox = ({ prices, handleFilters }) => {
   const [value, setValue] = useState(0);
-  const handleChange = () => {
-    //
+
+  const handleChange = (event) => {
+    handleFilters(event.target.value);
+    setValue(event.target.value);
   };
 
   return prices.map((price, index) => (
@@ -11,6 +13,7 @@ const Radiobox = ({ prices }) => {
       <input
         onChange={handleChange}
         value={`${price._id}`}
+        name={price}
         type="radio"
         className="mr-2 ml-4"
       />
--- client/src/core/Shop.js
+++ client/src/core/Shop.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Layout from './Layout';
 import Card from './Card';
-import { getCategories } from './apiCore';
+import { getCategories, getFilteredProducts } from './apiCore';
 import Checkbox from './Checkbox';
 import Radiobox from './Radiobox';
 import { prices } from './fixedPrices';
@@ -9,6 +9,10 @@ import { prices } from './fixedPrices';
 const Shop = () => {
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState(false);
+  const [limit, setLimit] = useState(6);
+  const [skip, setSkip] = useState(0);
+  const [size, setSize] = useState(0);
+  const [filteredResults, setFilteredResults] = useState(0);
   const [myFilters, setMyFilters] = useState({
     filters: { category: [], price: [] },
   });
@@ -23,14 +27,69 @@ const Shop = () => {
     });
   };
 
+  const loadFilteredResults = (newFilters) => {
+    getFilteredProducts(skip, limit, newFilters).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults(data.data);
+        setSize(data.size);
+        setSkip(0);
+      }
+    });
+  };
+
+  const loadMore = () => {
+    const toSkip = skip + limit;
+    getFilteredProducts(toSkip, limit, myFilters.filters).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults([...filteredResults, ...data.data]);
+        setSize(data.size);
+        setSkip(toSkip);
+      }
+    });
+  };
+
+  const loadMoreButton = () => {
+    return (
+      size > 0 &&
+      size >= limit && (
+        <button onClick={loadMore} className="btn btn-warning mb-5">
+          Load More
+        </button>
+      )
+    );
+  };
+
   useEffect(() => {
     init();
+    loadFilteredResults(skip, limit, myFilters.filters);
   }, []);
 
   const handleFilters = (filters, filterBy) => {
     const newFilters = { ...myFilters };
     newFilters.filters[filterBy] = filters;
+
+    if (filterBy == 'price') {
+      let priceValues = handlePrice(filters);
+      newFilters.filters[filterBy] = priceValues;
+    }
+    loadFilteredResults(myFilters.filters);
     setMyFilters(newFilters);
+  };
+
+  const handlePrice = (value) => {
+    const data = prices;
+    let array = [];
+
+    for (let key in data) {
+      if (data[key]._id === parseInt(value)) {
+        array = data[key].array;
+      }
+    }
+    return array;
   };
 
   return (
@@ -57,7 +116,16 @@ const Shop = () => {
             />
           </div>
         </div>
-        <div className="col-8">right area</div>
+        <div className="col-8">
+          <h2 className="mb-4">Products</h2>
+          <div className="row">
+            {filteredResults.map((product, index) => (
+              <Card key={index} product={product} />
+            ))}
+          </div>
+          <hr />
+          {loadMoreButton()}
+        </div>
       </div>
     </Layout>
   );
