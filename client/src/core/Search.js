a2e04df834c06193d5adf8b4264c8a5d6eaefe2c
Adam Dubey <adubey0311@gmail.com>
2022-03-10 08:13:49 -0500

    [AD][03M1022] Wrap up search functionality on client side

--- client/src/core/Search.js
+++ client/src/core/Search.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getCategories } from './apiCore';
+import { getCategories, list } from './apiCore';
 import Card from './Card';
 
 const Search = () => {
@@ -27,12 +27,49 @@ const Search = () => {
     loadCategories();
   }, []);
 
-  const searchSubmit = () => {
-    //
+  const searchData = () => {
+    if (search) {
+      list({ search: search || undefined, category: category }).then(
+        (response) => {
+          if (response.error) {
+            console.log(response.error);
+          } else {
+            setData({ ...data, results: response, searched: true });
+          }
+        },
+      );
+    }
   };
 
-  const handleChange = () => {
-    //
+  const searchSubmit = (event) => {
+    event.preventDefault();
+    searchData();
+  };
+
+  const handleChange = (name) => (event) => {
+    setData({ ...data, [name]: event.target.value, searched: false });
+  };
+
+  const searchMessage = (searched, results) => {
+    if (searched && results.length > 0) {
+      return `Found ${results.length} products!`;
+    }
+    if (searched && results.length < 1) {
+      return `No products found`;
+    }
+  };
+
+  const searchedProducts = (results = []) => {
+    return (
+      <div>
+        <h2 className="mt-4 mb-4">{searchMessage(searched, results)}</h2>
+        <div className="row">
+          {results.map((product, index) => (
+            <Card key={index} product={product} />
+          ))}
+        </div>
+      </div>
+    );
   };
 
   const searchForm = () => (
@@ -41,7 +78,7 @@ const Search = () => {
         <div className="input-group input-group-lg">
           <div className="input-group-prepend">
             <select className="btn mr-2" onChange={handleChange('category')}>
-              <option value="All">Pick Category</option>
+              <option value="All">All</option>
               {categories.map((category, index) => (
                 <option key={index} value={category._id}>
                   {category.name}
@@ -66,6 +103,7 @@ const Search = () => {
   return (
     <div className="row">
       <div className="container mb-3">{searchForm()}</div>
+      <div className="container-fluid mb-3">{searchedProducts(results)}</div>
     </div>
   );
 };
