69aca9d12238dc96fa5b65b726c1c90c16f5b9e8
Adam Dubey <adubey0311@gmail.com>
2022-03-11 15:50:36 -0500

    [AD][03M1122] Refactoring Card component

--- client/src/core/Home.js
+++ client/src/core/Home.js
@@ -45,14 +45,18 @@ const Home = () => {
 
       <div className="row">
         {productsByArrival.map((product, index) => (
-          <Card key={index} product={product} />
+          <div key={index} className="col-4 mb-3">
+            <Card product={product} />
+          </div>
         ))}
       </div>
 
       <h2 className="mb-4">Best Sellers</h2>
       <div className="row">
         {productsBySell.map((product, index) => (
-          <Card key={index} product={product} />
+          <div key={index} className="col-4 mb-3">
+            <Card product={product} />
+          </div>
         ))}
       </div>
     </Layout>
