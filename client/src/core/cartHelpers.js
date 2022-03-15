0a4622c69e62fb3409a0f95ac487a72c2c53bbc3
Adam Dubey <adubey0311@gmail.com>
2022-03-15 08:12:03 -0400

    [AD][03M1522] Implement initial addToCart feature

---
+++ client/src/core/cartHelpers.js
@@ -0,0 +1,22 @@
+export const addItem = (item, next) => {
+  let cart = [];
+  if (typeof window !== 'undefined') {
+    if (localStorage.getItem('cart')) {
+      cart = JSON.parse(localStorage.getItem('cart'));
+    }
+    cart.push({
+      ...item,
+      count: 1,
+    });
+    // rm duplicates
+    cart = Array.from(new Set(cart.map((product) => product._id))).map((id) => {
+      return cart.find((product) => product._id === id);
+    });
+    localStorage.setItem('cart', JSON.stringify(cart));
+    next();
+  }
+};
+
+export const updateItem = () => {}
+
+export const removeItem = () => {}
