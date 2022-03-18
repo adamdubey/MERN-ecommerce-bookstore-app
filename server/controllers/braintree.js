43d3bffa7b698b1383ac24b1b74a115788659364
Adam Dubey <adubey0311@gmail.com>
2022-03-18 08:40:39 -0400

    [AD][03M1822] Updating server-side  raintree controller

--- server/controllers/braintree.js
+++ server/controllers/braintree.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 
 const gateway = braintree.connect({
   environment: braintree.Environment.Sandbox,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
   merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY
 });
+
 exports.generateToken = (req, res) => {
   gateway.clientToken.generate({}, function (err, response) {
     if (err) {
