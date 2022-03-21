e3b24f9f33b7bf9a05ab07e41bc110c06ff5d62f
Adam Dubey <adubey0311@gmail.com>
2022-03-21 08:17:34 -0400

    [AD][03M2122] Updating Server-side payment controller

--- server/controllers/braintree.js
+++ server/controllers/braintree.js
@@ -2,11 +2,11 @@ const User = require('../models/user');
 const braintree = require('braintree');
 require('dotenv').config();
 
-const gateway = braintree.connect({
+const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
   merchantId: process.env.BRAINTREE_MERCHANT_ID,
   publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
 });
 
 exports.generateToken = (req, res) => {
@@ -17,4 +17,25 @@ exports.generateToken = (req, res) => {
       res.send(response);
     }
   });
+};
+
+exports.processPayment = (req, res) => {
+  let nonceFromTheClient = req.body.paymentMethodNonce;
+  let amountFromTheClient = req.body.amount;
+  let newTransaction = gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+      options: {
+        submitForSettlement: true,
+      },
+    },
+    (error, result) => {
+      if (error) {
+        res.status(500).json(error);
+      } else {
+        res.json(result);
+      }
+    },
+  );
 };
