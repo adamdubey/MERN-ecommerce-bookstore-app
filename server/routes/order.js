f38e1ea53378f5ae617a110a93936a4cb7d3f358
Adam Dubey <adubey0311@gmail.com>
2022-03-23 09:47:30 -0400

    [AD][03M2322] Implementing product orders inventory

--- server/routes/order.js
+++ server/routes/order.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const { requireSignin, isAuth } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
+const { userById, addOrderToUserHistory } = require('../controllers/user');
 const { create } = require('../controllers/order');
+const { decreaseQuantity } = require('../controllers/product');
 
-router.post('/order/create/:userId', requireSignin, isAuth, create);
+router.post(
+  '/order/create/:userId',
+  requireSignin,
+  isAuth,
+  addOrderToUserHistory,
+  decreaseQuantity,
+  create,
+);
 
 router.param('userId', userById);
 
