83f19d22c0fc4af1c802945521604619433cb319
Adam Dubey <adubey0311@gmail.com>
2022-03-28 08:14:55 -0400

    [AD][03M2822] Adding client-side Admin CRUD ops for managing products

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -11,6 +11,7 @@ import PrivateRoute from './auth/PrivateRoute';
 import AddCategory from './admin/AddCategory';
 import AddProduct from './admin/AddProduct';
 import Orders from './admin/Orders';
+import ManageProducts from './admin/ManageProducts';
 import Shop from './core/Shop';
 import Product from './core/Product';
 import Cart from './core/Cart';
@@ -31,6 +32,7 @@ const Routes = () => {
         <AdminRoute path="/create/category" exact component={AddCategory} />
         <AdminRoute path="/create/product" exact component={AddProduct} />
         <AdminRoute path="/admin/orders" exact component={Orders} />
+        <AdminRoute path="/admin/products" exact component={ManageProducts} />
       </Switch>
     </BrowserRouter>
   );
